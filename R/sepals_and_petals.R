d04c83a956cda6728e3820d34fd21f66fcfba18e
retowyss <reto.wyss.ch@gmail.com>
2018-01-25 08:25:33 +0100

    Init

---
+++ R/sepals_and_petals.R
@@ -0,0 +1,18 @@
+#' Plot Sepals and Petals in Iris
+#'
+#' @param x value in \code{names(iris)[1:4]}
+#' @param y value in \code{names(iris)[1:4]}
+#'
+#' @import ggplot2
+#' @import datasets
+#'
+#' @return ggplot object
+#' @export
+#'
+#' @examples
+#' sepals_and_petals("Sepal.Length", "Petal.Length")
+#'
+sepals_and_petals <- function(x, y) {
+  ggplot(datasets::iris, aes_string(x = x, y = y, color = "Species")) +
+    geom_point()
+}
